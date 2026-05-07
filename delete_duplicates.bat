da2adcc4d9840977754feb0c25fa0fae1f2d47a0
Vinayak Chinnarathod <vinayakchinnarathod@gmail.com>
2026-05-08 01:19:47 +0530

    backend upload

--- delete_duplicates.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "c:\Users\VINAYAK CHINNARATHOD\Documents\Bank System Website\backend\src\main\java\com\bank\securebank\controller"
-echo Deleting duplicate AdminController files...
-del /f /q "AdminController-FIXED.java" 2>nul
-del /f /q "AdminController-SIMPLE.java" 2>nul
-echo Done!
-dir *.java
-pause
