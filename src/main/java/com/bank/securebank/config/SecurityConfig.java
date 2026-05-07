d19b6cb75012fdb11bf56c17b82dd389756a126e
Vinayak Chinnarathod <vinayakchinnarathod@gmail.com>
2026-05-08 02:25:16 +0530

    removed jwt filter

--- src/main/java/com/bank/securebank/config/SecurityConfig.java
+++ src/main/java/com/bank/securebank/config/SecurityConfig.java
@@ -2,18 +2,9 @@ package com.bank.securebank.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -23,47 +14,12 @@ public class SecurityConfig {
 
         http
             .csrf(csrf -> csrf.disable())
-
-            .cors(cors -> {})
-
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/**").permitAll()
+                .anyRequest().permitAll()
             )
-
             .formLogin(form -> form.disable())
-
             .httpBasic(httpBasic -> httpBasic.disable());
 
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOriginPatterns(List.of("*"));
-
-        configuration.setAllowedMethods(
-                List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        );
-
-        configuration.setAllowedHeaders(List.of("*"));
-
-        configuration.setAllowCredentials(false);
-
-        configuration.setMaxAge(3600L);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
